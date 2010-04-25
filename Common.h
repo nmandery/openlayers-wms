e176e25d24df7a409555835d13731baeef9b4e22
Nico Mandery <nico@nmandery.net>
2010-04-25 22:05:37 +0200

    added common.h and added more paths to gitgnore

---
+++ Common.h
@@ -0,0 +1,22 @@
+// common types and defines
+
+/*
++-------------------------------------------------+
+|  defines                                        |
++-------------------------------------------------+
+*/
+
+#define XML_HEADER "<?xml version='1.0' encoding=\"UTF-8\" standalone=\"no\" ?>\n"
+
+/*
++-------------------------------------------------+
+|  types                                          |
++-------------------------------------------------+
+*/
+
+typedef struct {
+  double left;
+  double top;
+  double right;
+  double bottom;
+} BBOX;
